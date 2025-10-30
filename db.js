357c664e8a3bab286afdc0ffa368e2ef17889ac7
Jonapalacio <jonapalacio06@gmail.com>
2025-10-30 16:18:54 +0800

    update .env & db.js

--- db.js
+++ db.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
   ssl: {
-    rejectUnauthorized: false, 
-  },
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
